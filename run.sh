086bb11c4c7f2e638f6ba579b44c9d08f6d8ab7c
Michael Dreher <Michael.Dreher42@gmail.com>
2021-08-21 15:49:24 +0200

    changed mapping of license dir added container name "ise"

--- run.sh
+++ run.sh
@@ -9,14 +9,16 @@ DISPLAY_POSTFIX=":1"
 #   /opt/Xilinx/14.7/ISE_DS/common/bin/lin64/xlcm
 cmd="-c /opt/Xilinx/14.7/ISE_DS/ISE/bin/lin64/ise"
 
+# mac address and hostname are set to get a constant ID for the node specific licenses
 docker run -it --rm \
     -v "${HOME}/Documents/Xilinx/code:/code" \
-    -v "${HOME}/Documents/Xilinx/lic:/xilinx" \
+    -v "${HOME}/Documents/Xilinx/lic:/code/.xilinx" \
     -e "DISPLAY=host.docker.internal${DISPLAY_POSTFIX}" \
-    -e XILINXD_LICENSE_FILE=/xilinx/Xilinx.lic \
+    -e XILINXD_LICENSE_FILE=/code/.xilinx/Xilinx.lic \
     -e HOME=/code \
     -u "$(id -u):$(id -g)" \
     --entrypoint "/opt/entrypoint.sh" \
     --mac-address=02:43:ac:11:ff:fe  \
     --hostname=xilinx \
+    --name ise \
     xilinx-ise $cmd
